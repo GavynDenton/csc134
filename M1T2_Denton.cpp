e7d79ea7c8132a2b0aa75be19dd2c3f2ea9c8e64
GavynDenton <157392816+GavynDenton@users.noreply.github.com>
2024-01-22 16:16:36 +0000

    Completed M1T2

---
+++ M1T2_Denton.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M1T2
+// Gavyn Dentong
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "My name is Gavyn Denton" << "\n";
+    cout <<"I am majoring in engineering" << endl;
+    
+    // Ask a question
+    string name;
+    cout << "Whats your name? ";
+    cin >> name;
+    
+    cout << "Nice to meet you, " << name << endl;
+
+    cout << endl << endl;
+    return 0;
+}
