744faf2f16d114ba169a5807f7519945e9f60945
GavynDenton <157392816+GavynDenton@users.noreply.github.com>
2024-01-29 16:36:12 +0000

    M2T1 - Finished

--- M2/M2T1_Denton.cpp
+++ M2/M2T1_Denton.cpp
@@ -7,12 +7,15 @@ Gavyn Denton
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 
 int main()
 {
     cout << "M2T1" << endl;
+    cout << "Thank you for dinning with us" << endl;
+    cout << "--------------------" << endl;
     // set up variables
     string meal = "Value Meal";
     double meal_price = 5.99;
@@ -21,11 +24,16 @@ int main()
     double total = 0;
 
     // do calcuations
+    tax_amount = meal_price * tax_percent;
+    total = meal_price + tax_amount;
 
     // pring the reciept
-    // TODO: fix decimals
+    // print this once to set the decimals to exactly 2
+    
+    cout << fixed << setprecision(2);
     cout << meal << "\t$" << meal_price << endl;
     cout << "Tax:" << "\t\t$" << tax_amount << endl;
+    cout << "--------------------" << endl;
     cout << "Total:" << "\t\t$" << total << endl;
 
     return 0;
