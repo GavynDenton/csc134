cf872599baf059eaf30e9323f88fe191c7ef1fd8
GavynDenton <157392816+GavynDenton@users.noreply.github.com>
2024-01-30 06:14:52 +0000

    M1HW1 - added everything exept quotes

---
+++ M1/M1HW_Denton.cpp
@@ -0,0 +1,20 @@
+/*
+CSC 134
+M1HW1 - Movie Talk
+Denton
+1/29/24
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    string movie_name = "Iron Man";
+    string year = "2008";
+    string box_off = "585.8 Million";
+    cout << "The movie " << movie_name;
+    cout << " released in " << year;
+    cout << " grosing over " << box_off << endl;
+    cout << " world wide and launched Robert Downey Junior back to stardom.";
+    cout << endl;
+    return 0;
+}
