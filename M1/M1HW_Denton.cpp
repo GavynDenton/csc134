6cf8dfafc52a9d31bbd32f4309477db8c1f9e187
GavynDenton <157392816+GavynDenton@users.noreply.github.com>
2024-01-31 05:12:41 +0000

    M1HW - All Done!

--- M1/M1HW_Denton.cpp
+++ M1/M1HW_Denton.cpp
@@ -8,13 +8,18 @@ Denton
 using namespace std;
 
 int main() {
-    string movie_name = "Iron Man";
+    string mov_n = "Iron Man";
     string year = "2008";
     string box_off = "585.8 Million";
-    cout << "The movie " << movie_name;
+    cout << "The movie " << mov_n;
     cout << " released in " << year;
     cout << " grosing over " << box_off << endl;
-    cout << " world wide and launched Robert Downey Junior back to stardom.";
-    cout << endl;
+    cout << "world wide and launched Robert Downey Junior back to stardom." << endl;
+    cout << " " << endl;
+    cout << "Some popular quotes from " << mov_n << " are." << endl;
+    cout << "\"Yeah, I can fly.\"" << endl;
+    cout << "\"Tony Stark was able to build this in A cave! With A Box Of Scraps!\"" << endl;
+    cout << "\"I am " << mov_n << ".\"" << endl;
+
     return 0;
 }
