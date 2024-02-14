b0100b00e835cb87218636559e26e7c6e137842a
GavynDenton <157392816+GavynDenton@users.noreply.github.com>
2024-02-14 16:38:34 +0000

    M2HW_Pizza - Done!

---
+++ M2/M2HW_Denton.cpp
@@ -0,0 +1,38 @@
+// Use as starter for any program
+// CSC 134
+// M2HW
+// Denton
+// DATE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variables
+    double guest;
+    double pizzas;
+    double slicespp;
+    double slicespg = 3;
+    double slices_left;
+    double Tslices;
+    double slices_ate;
+
+    // User imput
+    cout << "How many pizzas did you order? ";
+    cin >> pizzas;
+    cout << "How many slices does each pizza have? ";
+    cin >> slicespp;
+    cout << "How many guests do you have? ";
+    cin >> guest;
+
+    // Calculate
+    Tslices = slicespp * pizzas;
+    slices_ate = guest * slicespg;
+    slices_left = Tslices - slices_ate;
+    //Print answers
+    cout << "You will have ";
+    cout << slices_left;
+    cout << " slices left over." << endl;
+
+    return 0;
+}
