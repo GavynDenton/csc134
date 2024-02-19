a9e89847e2e92c387e1ed362f4d684761e0bcd9a
GavynDenton <157392816+GavynDenton@users.noreply.github.com>
2024-02-19 00:16:10 +0000

    M2HW_q4 - done

--- M2/M2HW_Q4_Denton.cpp
+++ M2/M2HW_Q4_Denton.cpp
@@ -2,5 +2,27 @@
 CSC-134
 M2HW Question
 Gavyn Denton
-
+2/18/24
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //define strings
+    string cheer = "Lets Go ";
+    string school = "FTCC";
+    string team = "Trogans";
+
+    //print cheer
+    cout << cheer;
+    cout << school << endl;
+    cout << cheer;
+    cout << school << endl;
+    cout << cheer;
+    cout << school << endl;
+    cout << cheer;
+    cout << team << endl;
+
+    return 0;
+}
